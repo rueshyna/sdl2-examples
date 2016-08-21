7ba257f18c18c1e9d27087fb7e7c7115386d2c99
Rueshyna <rueshyna@gmail.com>
2016-08-21 23:00:47 +0800

    add error handling and some comment

--- src/Lesson01.hs
+++ src/Lesson01.hs
@@ -6,24 +6,48 @@ import qualified SDL
 import Linear.V4 (V4(..))
 --
 import Control.Concurrent (threadDelay)
+import Control.Exception
 --
 import qualified Config
 --
+import System.Exit (die)
+
 lesson01 :: IO ()
 lesson01 = do
-   SDL.initialize [SDL.InitVideo]
-   window <- SDL.createWindow "Lesson01" Config.winConfig
-   SDL.showWindow window
+   -- initialize SDL
+   run (SDL.initialize [SDL.InitVideo])
+       "SDL could not initialize!"
+
+   -- create window
+   window <- run (SDL.createWindow "Lesson01" Config.winConfig)
+                 "Window could not be created!"
+
    -- get surface from given window
    gSurface <- SDL.getWindowSurface window
+
    -- fill the global surface with black
    SDL.surfaceFillRect gSurface Nothing $
       -- setting color with R-G-B-A
       V4 maxBound maxBound minBound maxBound
+
    -- update the surface for a specific window
    SDL.updateWindowSurface window
-   --
+
+   -- wait two seconds :
+   -- https://hackage.haskell.org/package/sdl2-2.1.2.1/docs/SDL-Time.html#v:delay
+   -- Users are generally recommended to use threadDelay instead, to take
+   -- advantage of the abilities of the Haskell runtime.
    threadDelay 2000000
-   --
+
+   -- destroy window
    SDL.destroyWindow window
    SDL.quit
+
+-- if something wrong then exit the program
+-- IO a   : SDL call
+-- String : error message
+run :: IO a -> String -> IO a
+run exec errMessage =
+    catch exec
+          (\e -> do let err = show (e :: IOException)
+                    die (errMessage ++ " SDL_Error: "++ err))
