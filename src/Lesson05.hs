aa3b56d3642ada063f58e3459af695f405689e47
Rueshyna <rueshyna@gmail.com>
2016-08-25 22:48:52 +0800

    modify example05

--- src/Lesson05.hs
+++ src/Lesson05.hs
@@ -3,13 +3,18 @@
 module Lesson05 where
 --
 import qualified SDL
-import Linear.V4 (V4(..))
+import Linear.V2 (V2(V2))
+import Linear.Affine(Point(P))
 --
 import Control.Concurrent (threadDelay)
 import Control.Monad (unless)
 import Control.Applicative ((<*))
+import Control.Exception (catch,IOException)
 --
 import qualified Config
+--
+import System.Exit (die)
+
 
 -- In fact, SDL converts color mode in every blitting if
 -- the color mode of source surface doesn't match
@@ -31,26 +36,40 @@ optLoadBMPwith originSf path = do
 --
 lesson05 :: IO ()
 lesson05 = do
-   SDL.initialize [SDL.InitVideo]
-   window <- SDL.createWindow "Lesson05" Config.winConfig
-   SDL.showWindow window
+   -- initialize SDL
+   run (SDL.initialize [SDL.InitVideo])
+       "SDL could not initialize!"
+
+   -- create window
+   window <- run (SDL.createWindow "Lesson05" Config.winConfig)
+                 "Window could not be created!"
+
    gSurface <- SDL.getWindowSurface window
-   sf <- optLoadBMPwith gSurface "./img/05/up.bmp"
+   sf <- run (optLoadBMPwith gSurface "./img/05/up.bmp")
+             "Failed to load stretching image"
    let
       loop = do
          events <- SDL.pollEvents
          let quit = any (== SDL.QuitEvent) $ map SDL.eventPayload events
-         SDL.surfaceFillRect gSurface Nothing $
-            V4 minBound minBound minBound maxBound
          -- blit with given scaling setup
          -- Nothing for default setup - blitting with fully filling
-         SDL.surfaceBlitScaled sf Nothing gSurface Nothing
+         SDL.surfaceBlitScaled sf Nothing gSurface
+            $ Just $ SDL.Rectangle (P (V2 0 0)) (V2 200 200)
          SDL.updateWindowSurface window
          threadDelay 20000
          unless quit loop
    loop
-   SDL.destroyWindow window
+
+   -- Free resources and close SDL
    SDL.freeSurface sf
+   SDL.destroyWindow window
    SDL.quit
 
 -- .
+
+-- if something wrong then exit the program
+run :: IO a -> String -> IO a
+run exec errMessage =
+    catch exec
+          (\e -> do let err = show (e :: IOException)
+                    die (errMessage ++ " SDL_Error: "++ err))
