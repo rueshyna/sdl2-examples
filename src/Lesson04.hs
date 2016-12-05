6acb57f9702f420b50a9737a18a58bef41e917f5
Rueshyna <rueshyna@gmail.com>
2016-12-05 23:45:42 +0800

    refine lesson04, using one-hole-context to set default value and loadable instance

--- src/Lesson04.hs
+++ src/Lesson04.hs
@@ -6,50 +6,101 @@ import qualified SDL
 --
 import Data.Monoid
 import Data.Maybe
+import Data.Foldable
+import Control.Applicative
+import Control.Monad.IO.Class (MonadIO)
+
 --
 import PixelPen
 --
+
+data Input = IDefault
+           | IDown
+           | IUp
+           | IRight
+           | ILeft
+           deriving (Show, Eq, Ord, Enum)
+
+data Mapping = M Input SDL.Keycode
+
+data Link a = L Input a
+
+preLink' :: State (Link String)
+preLink' = S [] ( L IDefault "./img/press.bmp")
+                [ L IDown    "./img/down.bmp"
+                , L IUp      "./img/up.bmp"
+                , L IRight   "./img/right.bmp"
+                , L ILeft    "./img/left.bmp"
+                ]
+
+data State a = S [a] a [a]
+
+mapping :: [ Mapping ]
+mapping = [ M IDefault SDL.KeycodeEscape
+          , M IDown    SDL.KeycodeDown
+          , M IUp      SDL.KeycodeUp
+          , M IRight   SDL.KeycodeRight
+          , M ILeft    SDL.KeycodeLeft
+          ]
+
+toInput :: [ Mapping ] -> SDL.Keycode -> Input
+toInput [] _ = IDefault
+toInput (M i k:xs) mk
+    | k == mk = i
+    | k /= mk = toInput xs mk
+
+class Moveable m where
+    left :: m a -> m a
+    right :: m a-> m a
+
+instance Moveable State where
+  left  (S [] c rs) = S [] c rs
+  left  (S (l:ls) c rs) = S ls l (c:rs)
+
+  right (S ls c []) = S ls c []
+  right (S ls c (r:rs)) = S (c:ls) r rs
+
+instance Functor State where
+  fmap f (S xs y zs) = S (map f xs) (f y) (map f zs)
+
+instance Foldable State where
+  foldr f w (S xs y zs) = foldr f (f y (foldr f w zs)) $ reverse xs
+
+instance Traversable State where
+  traverse f (S xs y zs) = liftA3 S (traverse f xs) (f y) (traverse f zs)
+
+instance Loadable State where
+  load = mapM
+  unload = mapM_
+
+instance Loadable Link where
+  load f (L k a) = L k <$> f a
+  unload f (L k a) = f a
 
 lesson04 :: IO ()
 lesson04
     = (^.^) sdlInitVideo ()                     -- initialize SDL
     $ \() -> (^.^) defaultWindow "Lesson04"     -- create window
     $ \w -> (^.^) surface w                     -- get surface from given window
-    -- load image file as a surface
-    $ \s -> (^.^) bmpSurface "./img/press.bmp"  -- default image
-    $ \p -> (^.^) bmpSurface "./img/up.bmp"     -- up image
-    $ \u -> (^.^) bmpSurface "./img/down.bmp"   -- down impage
-    $ \d -> (^.^) bmpSurface "./img/left.bmp"   -- left impage
-    $ \l -> (^.^) bmpSurface "./img/right.bmp"  -- right image
-    $ \r -> update_ p $ eventHandle_ checkDefaultQuit
+    $ \s -> (^.^/) bmpSurface preLink'
+    $ \l@(S xs y@(L k sur) zs) -> update_ sur $ eventHandle_ checkDefaultQuit
       (\f t -> do
-               let newS = eventToSurface u d l r p $ SDL.eventPayload f
-               return $ fromMaybe t newS
+               let newS = eventToSurface l $ SDL.eventPayload f
+               return $ fromMaybe sur newS
       )
       (\f t -> do
                SDL.surfaceBlit t Nothing s Nothing
                SDL.updateWindowSurface w
       )
+
 -- to decide a surface to blit from given event info.
-eventToSurface :: SDL.Surface -- for up-event
-               -> SDL.Surface -- for down-event
-               -> SDL.Surface -- for left-event
-               -> SDL.Surface -- for right-event
-               -> SDL.Surface -- for default
+eventToSurface :: State (Link SDL.Surface)
                -> SDL.EventPayload
                -> Maybe SDL.Surface
-eventToSurface picUp
-               picDown
-               picLeft
-               picRight
-               picDefault
-               (SDL.KeyboardEvent ked) =
-   case (SDL.keysymKeycode $ SDL.keyboardEventKeysym ked) of
-      SDL.KeycodeUp     -> Just picUp
-      SDL.KeycodeDown   -> Just picDown
-      SDL.KeycodeLeft   -> Just picLeft
-      SDL.KeycodeRight  -> Just picRight
-      SDL.KeycodeEscape -> Just picDefault
-      otherwise        -> Nothing
--- if input event is not KeyboardEvent then return Nothing
-eventToSurface _ _ _ _ _ _      = Nothing
+eventToSurface s@(S xs y@(L i sur) zs) event@(SDL.KeyboardEvent ked)
+    | i < input = eventToSurface (right s) event
+    | i > input = eventToSurface (left s) event
+    | i == input = Just sur
+    where input = toInput mapping $ SDL.keysymKeycode $ SDL.keyboardEventKeysym ked
+
+eventToSurface _ _  = Nothing
